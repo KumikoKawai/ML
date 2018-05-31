422574bb9b6ee387f96e912871232d393334cd94
liuyuhan-ch98 <39582956+liuyuhan-ch98@users.noreply.github.com>
2018-05-31 14:00:29 +0900

    Add files via upload
    translate Note

--- ngramtest.py
+++ ngramtest.py
@@ -2,49 +2,66 @@ import urllib.request
 import re
 import string
 import operator
+import csv
 
 def cleanText(input):
-    input = re.sub('\n+', " ", input).lower() # 匹配换行,用空格替换换行符
-    input = re.sub('\[[0-9]*\]', "", input) # 剔除类似[1]这样的引用标记
-    input = re.sub(' +', " ", input) #  把连续多个空格替换成一个空格
-    #input = bytes(input)#.encode('utf-8') # 把内容转换成utf-8格式以消除转义字符
+    input = re.sub('\n+', " ", input).lower() # change line &Transcoding mark change to Empty case
+    input = re.sub('\[[0-9]*\]', "", input) # Removal of similarity [1] ferential quotation mark
+    input = re.sub(' +', " ", input) #change two or more empty cases to one empty case
+    #input = bytes(input)#.encode('utf-8') # contents change to utf-8 formal
     #input = input.decode("ascii", "ignore")
     return input
 
 def cleanInput(input):
     input = cleanText(input)
     cleanInput = []
-    input = input.split(' ') #以空格为分隔符，返回列表
+    input = input.split(' ') # split with empty case, return result
 
     for item in input:
-        item = item.strip(string.punctuation) # string.punctuation获取所有标点符号
+        item = item.strip(string.punctuation) # string.punctuation gain all sign
 
-        if len(item) > 0 or (item.lower() == 'a' or item.lower() == 'i'): #找出单词，包括i,a等单个单词
+        if len(item) > 0 or (item.lower() == 'a' or item.lower() == 'i'): #find Lonely
             cleanInput.append(item)
     return cleanInput
 
 def getNgrams(input, n):
     input = cleanInput(input)
 
-    output = {} # 构造字典
+    output = {} # Structure dictionary
     for i in range(len(input)-n+1):
         ngramTemp = " ".join(input[i:i+n])#.encode('utf-8')
-        if ngramTemp not in output: #词频统计
-            output[ngramTemp] = 0 #典型的字典操作
+        if ngramTemp not in output: # calculate code frequency
+            output[ngramTemp] = 0 
         output[ngramTemp] += 1
     return output
 
-
+def createListCSV(fileName="", dataList=[]):
+    with open(fileName, "w",newline='') as csvFile:
+        csvWriter = csv.writer(csvFile,delimiter=',')
+        for data in dataList:
+            csvWriter.writerow(data)
+            csvFile.close
 
 
 #url = 'http://pythonscraping.com/files/inaugurationSpeech.txt'
 #data = urllib.request.urlopen(url).read()
-data = open("/Users/yuhanliu/Downloads/test1127/changetosignal.txt").read()
+data = open("/Users/yuhanliu/Downloads/test1127/under2changetosignal.txt").read()
 #data = data.decode('utf-8')
-rams = getNgrams(data, 3)
-sortedNGrams = sorted(rams.items(), key = operator.itemgetter(1), reverse=True) #=True 降序排列
+rams = getNgrams(data, 4)
+sortedNGrams = sorted(rams.items(), key = operator.itemgetter(1), reverse=True) #=True An interjection
 
-print(sortedNGrams)
-#f = open('/Users/yuhanliu/Downloads/ngram-result.txt', 'w') # 若是'wb'就表示写二进制文件
-#f.write(sortedNGrams)
-#f.close()
+#print(sortedNGrams)
+#file = open('/Users/yuhanliu/Downloads/ngram-result.txt', 'w') # write file
+#file.write(str(sortedNGrams));  
+#file.close() 
+
+#open file，add a
+
+createListCSV('/Users/yuhanliu/Downloads/under2-4-gramtest.csv', sortedNGrams)
+#out = open('/Users/yuhanliu/Downloads/ngramtest.csv','a', newline='')
+
+#csv_write = csv.writer(out,dialect='excel')
+#write contents
+#sv_write.writerow(str(sortedNGrams))
+
+print ("write over")
